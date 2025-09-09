69c14e7eeabd72f236f71c63ab27a317a2e4d168
pedrosr-28 <pedrosr2112@gmail.com>
2025-09-09 13:25:59 +0200

    Create locations.py
    Ubicaciones para las resoluciones de los mapas de calor

---
+++ locations.py
@@ -0,0 +1,41 @@
+        if ciudad == 'Cali':
+            esquinaSuperiorIzquierda = (3.523918, -76.575961)
+            esquinaInferiorDerecha = (3.279561, -76.4568)
+            lat_num_step = 80
+            lon_num_step = 40
+        elif ciudad == 'Bogota':
+            esquinaSuperiorIzquierda = (4.824446174896161, -74.20667507828892)
+            esquinaInferiorDerecha = (4.5397692048476825, -74.0005320015009)
+            lat_num_step = 70
+            lon_num_step = 50
+        elif ciudad == 'Medellin':
+            esquinaSuperiorIzquierda = (6.349131881712998, -75.65659770930033)
+            esquinaInferiorDerecha = (6.155727548907033, -75.48383560122318)
+            lat_num_step = 50
+            lon_num_step = 50
+        elif ciudad == 'Madrid':
+            esquinaSuperiorIzquierda = (40.99513263469628, -4.323253097664286)
+            esquinaInferiorDerecha = (40.10050165392667, -3.194924166916184)
+            lat_num_step = 50
+            lon_num_step = 50
+        elif ciudad == 'Cartagena':
+            esquinaSuperiorIzquierda = (37.709116757663374, -1.0969966393871564)
+            esquinaInferiorDerecha = (37.56502284133423, -0.9054225377768926)
+            lat_num_step = 30
+            lon_num_step = 30
+        elif ciudad == 'Murcia':
+            esquinaSuperiorIzquierda = (38.51625748003036, -1.9646254875938247)
+            esquinaInferiorDerecha = (37.58196804950511, -0.8258322339837023)
+            lat_num_step = 50
+            lon_num_step = 50
+        elif ciudad == 'Sevilla':
+            esquinaSuperiorIzquierda = (37.46163310797518, -6.067321540264034)
+            esquinaInferiorDerecha = (37.29921783810181, -5.822141287335516)
+            lat_num_step = 50
+            lon_num_step = 50
+        elif ciudad == 'London':
+            esquinaSuperiorIzquierda = (51.63363484575785, -0.523631062229996)
+            esquinaInferiorDerecha = (51.422331505996674, 0.7496885004871515)
+            lat_num_step = 25
+            lon_num_step = 95
+ 
